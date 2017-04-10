67e746d87be3e754b901e2754b573a8cf031b67c
maciej <maciekpaluch123@gmail.com>
2017-04-10 02:40:59 +0200

    lab2

--- lab2/src/dv2rpy.cpp
+++ lab2/src/dv2rpy.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <cmath>
 
 int main()
 {
@@ -10,7 +11,7 @@ int main()
 	float tab[i][4];
 	for(int k=0;k<i;++k)
 	{
-		std::cout << "--------" << std::endl;
+		std::cout << "-------- i=" << k+1 << std::endl;
 		std::cout << "a_=";
 		std::cin >> tab[k][0];
 		std::cout << std::endl;
@@ -24,7 +25,19 @@ int main()
 		std::cin >> tab[k][3];
 		std::cout << std::endl;
 	}
-	float tan_psi = (sin(tab[1][2]))/(cos(tab[1][2]));
+	for (int o=0; o<i; ++o)
+	{	
+		float cos_theta = cos(tab[o][3]);
+		float sin_theta = sin(tab[o][3]);
+		float cos_alpha = cos(tab[o][2]);
+		float sin_alpha = sin(tab[o][2]);
+
+		float fi = atan2(cos_theta*sin_alpha, cos_alpha);
+		float theta = atan2(-1*sin_theta*sin_alpha, sqrt(1 - cos_theta*sin_alpha*cos_theta*sin_alpha));
+		float psi = atan2(sin_theta*cos_alpha, cos_theta);
+
+		std::cout << "roll: " << psi << " pitch: " << theta << " yaw: " << fi << std::endl;
+	}
 }
 		
 
